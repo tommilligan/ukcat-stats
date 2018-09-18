e559f2d735d084a5bce4a229905e9fc068bd2e7b
Tom Milligan <tommilligan@users.noreply.github.com>
2018-09-18 14:12:51 +0100

    inverted data

--- plot.py
+++ plot.py
@@ -1,20 +1,25 @@
 import bokeh.plotting as bk
 from bokeh.models import Span
-from bokeh.io import export_png
+from bokeh.io import export_png, show, output_notebook
 import numpy as np
 from scipy.interpolate import CubicSpline
 
 SCORE = 3000
+X_RANGE = (2000, 3200)
 
-def find_nearest(array, value):
-    array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin()
-    return idx
+def isnotebook():
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            # Jupyter notebook or qtconsole
+            return True   
+    except NameError:
+        pass
+    # standard python, ipython, etc.
+    return False
 
-# ukcat deciles
-x = np.linspace(0.1, 0.9, 9)
 # code scores
-y = [
+x = [
     2220,
     2340,
     2420,
@@ -25,15 +30,17 @@ y = [
     2760,
     2870
 ]
+# ukcat deciles
+y = np.linspace(0.1, 0.9, 9)
 
 # Create plot
-p = bk.figure(x_range=(0, 1), y_range=(2000, 3200), title="UKCAT Interim Results 2018")
+p = bk.figure(x_range=X_RANGE, y_range=(0, 1), title="UKCAT Interim Results 2018")
 
 # plot initial data points
 p.circle(x, y, color='grey')
 
 # Generate x points for a smooth curve
-xvals=np.linspace(0, 1, 100)
+xvals=np.linspace(min(x), max(x), 100)
 
 # bezier curve to fit points exactly
 spl = CubicSpline(x, y) # First generate spline function
@@ -43,14 +50,14 @@ p.line(xvals, y_smooth, color='grey')
 
 # Nadia!
 # Calculate lines
-hline = Span(location=SCORE, dimension='width', line_color='red', line_width=1)
-percentile = xvals[find_nearest(y_smooth, SCORE)]
-vline = Span(location=percentile, dimension='height', line_color='red', line_width=1, line_dash='dashed')
-p.renderers.extend([hline, vline])
+vline = Span(location=SCORE, dimension='height', line_color='red', line_width=1)
+p.renderers.extend([vline])
 # Add text
-p.text([0], [SCORE], [" Nadia"], text_baseline="bottom", text_align="left")
-p.text([percentile], [2000], ["~{0:.2f}% ".format(percentile)], text_baseline="bottom", text_align="right")
+p.text([SCORE], [0], ["Nadia "], text_baseline="bottom", text_align="right")
 
-
-export_png(p)
+if isnotebook():
+    output_notebook()
+    show(p)
+else:
+    export_png(p)
 
